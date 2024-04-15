eb3b07b7bd4f0e9035ea974044bc3cd00337a043
Dan Toms <157254317+pynmash@users.noreply.github.com>
2024-04-15 23:29:33 +0100

    Add classes

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
+import pandas as pd
+
+df = pd.read_csv("hotels.csv")
+
+
 class Hotel:
+    def __init__(self, hotel_id):
+        pass
+
     def book(self):
         pass
 
     def view_hotels(self):
         pass
 
+    def available(self):
+        pass
+
 
 class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        pass
+
     def generate(self):
         pass
+
+
+print(df)
+hotel_id = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_id)
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(name, hotel)
+    print(reservation_ticket.generate())
+else:
+    print("Hotel is not free")
